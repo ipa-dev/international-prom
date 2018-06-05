2b0e81046e4269c4a55306c27df19b95ed92926f
Mason Watson <Mason@Masons-MacBook-Air.local>
2018-06-05 13:02:20 -0400

    stop message sending for a moment

--- cronjob_post.php
+++ cronjob_post.php
@@ -43,7 +43,7 @@ $cal_post_event_data = new WP_Query($post_args);
 
 if($cal_post_event_data->have_posts()){
 	while($cal_post_event_data->have_posts()) : $cal_post_event_data->the_post();
-		
+
 		$cal_post_event_id = get_the_ID();
 		$post_author_id = get_post_field( 'post_author', $cal_post_event_id );
 		$type = get_post_meta($cal_post_event_id, 'type', true);
@@ -84,7 +84,7 @@ if($cal_post_event_data->have_posts()){
 						), $facebook_access_token );
 						$post = $res->getGraphObject();
 					} else {
-						
+
 						$res  = $fb->post( '/'.$active_page.'/feed', array(
 							'message'           => wp_strip_all_tags(get_the_content())
 						), $facebook_access_token );
@@ -229,13 +229,14 @@ if($cal_post_event_data->have_posts()){
 						if(!empty($phoneArray)) {
 							foreach ( $phoneArray as $customer_phone ) {
 								if ( ! empty( $customer_phone ) ) {
-									$sms = $client->account->messages->create(
+									var_dump($customer_phone);
+									/*$sms = $client->account->messages->create(
 										$customer_phone,
 										array(
 											'from' => '+18652400405',
 											'body' => $text
 										)
-									);
+									);*/
 								}
 							}
 						}
@@ -310,7 +311,7 @@ foreach($users_array as $user_id){
 					//echo 'aa';
 						$active_page = get_user_meta($post_author_id, 'active_page', TRUE);
 						$facebook_access_token = get_user_meta( $post_author_id, 'facebook_access_token', true );
-						
+
 						if ( ! empty( $facebook_access_token ) && ! empty($active_page)) {
 							if ( $active_page != 'me ') {
 								$response = $fb->get('/'.$active_page.'?fields=access_token', $facebook_access_token);
@@ -351,7 +352,7 @@ foreach($users_array as $user_id){
 							}
 						}
 				}
-				
+
 				if($type == "twitter") {
 					$twitter_access_token = get_user_meta( $post_author_id, 'twitter_access_token', true );
 					if ( ! empty( $twitter_access_token ) ) {
@@ -375,14 +376,14 @@ foreach($users_array as $user_id){
 					if ( ! empty( $pinterest_access_token ) ) {
 						$ch = curl_init( 'https://api.pinterest.com/v1/me/search/boards/?query=internationalprom&access_token=' . $pinterest_access_token . '&fields=id' );
 						curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-						
+
 						$result = json_decode( curl_exec( $ch ) );
 						var_dump($result);
 						if ( ! empty( $result->data ) ) {
 							$board = $result->data[0]->id;
-							
+
 						} else {
-							
+
 							$ch = curl_init( 'https://api.pinterest.com/v1/boards/?access_token=' . $pinterest_access_token . '&fields=id' );
 							curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "POST" );
 							curl_setopt( $ch, CURLOPT_POSTFIELDS, array(
