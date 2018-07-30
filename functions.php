741af4268a3d60ae569a76971c0e3fb15491d3f1
Mason Watson <mkw@Masons-MacBook-Pro.local>
2018-07-30 15:49:18 -0400

    get more lists

--- functions.php
+++ functions.php
@@ -1890,6 +1890,7 @@ function dropPopupEdit() {
                     <?php
                     $data = array(
 	                    'fields' => 'lists',
+											'count' => '-1',
                     );
 
                     $url = get_user_meta($user_ID, 'mailchimp_endpoint', true).'/3.0/lists/';
@@ -2313,6 +2314,7 @@ function dropPopup() {
                     <?php
                     $data = array(
 	                    'fields' => 'lists',
+											'count' => '-1',
                     );
 
                     $url = get_user_meta($user_ID, 'mailchimp_endpoint', true).'/3.0/lists/';
