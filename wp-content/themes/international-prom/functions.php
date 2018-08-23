7d041ad3457b81136ba02bca8373067b6c1aed5d
Mason Watson <mkw@Masons-MBP.netgear.com>
2018-08-23 17:44:17 -0400

    work on angular calendar

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -991,7 +991,7 @@ function contentRefresh() {
 					<?php
 					if ( $i == $page ) {
 						?>
-						 class="active" 
+						 class="active"
 						 <?php
 					} else {
 						?>
@@ -1138,7 +1138,7 @@ function ImgPopContent() {
 										<?php
 										if ( $i == 1 ) {
 											?>
-											 class="active" 
+											 class="active"
 											 <?php
 										} else {
 											?>
@@ -1208,7 +1208,7 @@ function ImgPopContent() {
 										<?php
 										if ( $i == 1 ) {
 											?>
-											 class="active" 
+											 class="active"
 											 <?php
 										} else {
 											?>
@@ -1476,7 +1476,7 @@ function fancyboxImageSelector() {
 								<?php
 								if ( $i == 1 ) {
 									?>
-									 class="active" 
+									 class="active"
 									 <?php
 								} else {
 									?>
@@ -2824,17 +2824,17 @@ function mm_scripts_basic() {
 
 		wp_enqueue_script( 'custom-angularjs', get_stylesheet_directory_uri() . '/node_modules/angular/angular.min.js' );
 		wp_enqueue_script( 'custom-calendar', get_stylesheet_directory_uri() . '/node_modules/angular-ui-calendar/src/calendar.js' );
-	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/custom-script.js', array( 'jquery', 'custom-angularjs' ), ( mt_rand( 10, 100 ) ) );
-	wp_enqueue_script( 'custom-fullcalendar', get_stylesheet_directory_uri() . '/js/fullcalendar.js' );
-	wp_enqueue_script( 'jquery-ui-mouse', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/jquery.ui.mouse.min.js' );
-	wp_enqueue_script( 'jquery-ui-draggable', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/jquery.ui.draggable.min.js' );
-	wp_enqueue_script( 'custom-moment', get_stylesheet_directory_uri() . '/js/moment.min.js' );
+		wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/custom-script.js', array( 'jquery', 'custom-angularjs' ), ( mt_rand( 10, 100 ) ) );
+		wp_enqueue_script( 'custom-fullcalendar', get_stylesheet_directory_uri() . '/node_modules/fullcalendar/fullcalendar.js', array( 'jquery', 'custom-moment') );
+		wp_enqueue_script( 'jquery-ui-mouse', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/jquery.ui.mouse.min.js' );
+		wp_enqueue_script( 'jquery-ui-draggable', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/jquery.ui.draggable.min.js' );
+		wp_enqueue_script( 'custom-moment', get_stylesheet_directory_uri() . '/node_modules/moment/min/moment.min.js' );
 		wp_enqueue_script( 'jquery-confirm', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js' );
 		wp_enqueue_script( 'jquery-tinymce', 'https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=ktzixf62qqu05yekd7dcoi1mzg3lqf7bl08zwtuzeuf1loc4' );
 		wp_enqueue_script( 'custom-grapes', get_stylesheet_directory_uri() . '/js/grapes.min.js' );
-		wp_enqueue_style( 'custom_fullcalendar', get_stylesheet_directory_uri() . '/css/fullcalendar.css' );
 
-	wp_enqueue_style( 'custom_socialMediaPost', get_stylesheet_directory_uri() . '/css/socialMediaPost.css' );
+		wp_enqueue_style( 'custom_fullcalendar', get_stylesheet_directory_uri() . '/css/fullcalendar.css' );
+		wp_enqueue_style( 'custom_socialMediaPost', get_stylesheet_directory_uri() . '/css/socialMediaPost.css' );
 		wp_enqueue_style( 'jquery_confirm', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css' );
 		wp_enqueue_style( 'custom_grapes', get_stylesheet_directory_uri() . '/css/grapes.min.css' );
 
