5f6d81c76286e7045abcb731cfdc90bdbe45c437
CoreTechSolution <tanujcoregen@gmail.com>
2018-08-29 12:54:11 +0530

    Revert Back the fix for js error

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -2822,13 +2822,13 @@ function mcal_action() {
 
 function mm_scripts_basic() {
 
-		wp_enqueue_script( 'custom-angularjs', get_stylesheet_directory_uri() . '/js/angular.js' );
-		wp_enqueue_script( 'custom-calendar', get_stylesheet_directory_uri() . '/js/calendar.js' );
+		wp_enqueue_script( 'custom-angularjs', get_stylesheet_directory_uri() . '/node_modules/angular/angular.min.js' );
+		wp_enqueue_script( 'custom-calendar', get_stylesheet_directory_uri() . '/node_modules/angular-ui-calendar/src/calendar.js' );
 		wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/custom-script.js', array( 'jquery', 'custom-angularjs' ), ( mt_rand( 10, 100 ) ) );
-		wp_enqueue_script( 'custom-fullcalendar', get_stylesheet_directory_uri() . '/js/fullcalendar.js', array( 'jquery', 'custom-moment') );
+		wp_enqueue_script( 'custom-fullcalendar', get_stylesheet_directory_uri() . '/node_modules/fullcalendar/fullcalendar.js', array( 'jquery', 'custom-moment') );
 		wp_enqueue_script( 'jquery-ui-mouse', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/jquery.ui.mouse.min.js' );
 		wp_enqueue_script( 'jquery-ui-draggable', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.2/jquery.ui.draggable.min.js' );
-		wp_enqueue_script( 'custom-moment', get_stylesheet_directory_uri() . '/js/moment.min.js' );
+		wp_enqueue_script( 'custom-moment', get_stylesheet_directory_uri() . '/node_modules/moment/min/moment.min.js' );
 		wp_enqueue_script( 'jquery-confirm', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js' );
 		wp_enqueue_script( 'jquery-tinymce', 'https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=ktzixf62qqu05yekd7dcoi1mzg3lqf7bl08zwtuzeuf1loc4' );
 		wp_enqueue_script( 'custom-grapes', get_stylesheet_directory_uri() . '/js/grapes.min.js' );
