c186b3891cf00d8b7d2105248a985b65b6fb2892
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-04 01:05:35 +0530

    Stripe SDK

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -834,9 +834,23 @@ add_action('wp_ajax_stripePayment', 'stripePayment');
 function stripePayment() {
     global $user_ID;
     require_once(TEMPLATEPATH.'/stripe-php/config.php');
-    if ($_POST['stripeAmount']) {
-        $amount   = $_POST['stripeAmount'] * 100;
-
+    if ($_REQUEST['stripeAmount']) {
+        $amount   = $_REQUEST['stripeAmount'] * 100;
+        $token    = $_REQUEST['stripeToken'];
+        $customer = \Stripe\Customer::create( array(
+            'email' => $_REQUEST['stripeEmail'],
+            'card'  => $token
+        ) );
+        $charge   = \Stripe\Charge::create( array(
+            'customer'    => $customer->id,
+            'amount'      => $amount,
+            'description' => 'Text credit of '.$_REQUEST['text_limit'].' texts',
+            'currency'    => 'usd'
+        ) );
+        if ( $charge ) {
+            update_user_meta($user_ID, 'text_limit', $_REQUEST['text_limit']);
+            echo get_user_meta($user_ID, 'text_limit', true);
+        }
     }
     wp_die();
 }
