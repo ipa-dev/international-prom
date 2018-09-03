3e14d4b921cd34f720ed85d25945b7f0dffc3f87
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-03 19:43:14 +0530

    Stripe Integration

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -843,12 +843,12 @@ function stripe_payment() {
         $charge   = \Stripe\Charge::create( array(
             'customer'    => $customer->id,
             'amount'      => $amount,
-            "description" => '',
+            'description' => '',
             'currency'    => 'usd'
         ) );
         if ( $charge ) {
             update_user_meta($user_ID, 'text_limit', $_POST['text_limit']);
-            echo get_user_meta( $user_ID, 'text_limit', true );
+            echo get_user_meta($user_ID, 'text_limit', true);
         }
     }
     wp_die();
