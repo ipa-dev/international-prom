3de62a7f51e4b4b6b0d553771cc624543c5a6a7c
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-04 01:04:44 +0530

    Stripe SDK

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -833,7 +833,11 @@ add_action('wp_ajax_stripePayment', 'stripePayment');
 
 function stripePayment() {
     global $user_ID;
-    echo $user_ID;
+    require_once(TEMPLATEPATH.'/stripe-php/config.php');
+    if ($_POST['stripeAmount']) {
+        $amount   = $_POST['stripeAmount'] * 100;
+
+    }
     wp_die();
 }
 
