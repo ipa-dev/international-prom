6fd03d37865e5a68eaa63bc1f6a544c530d6a253
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-03 15:49:05 +0530

    Twilio text msg price field

--- wp-content/themes/international-prom/phpmodule_includes/EditProfileAccount/ModuleTextEditAccountForm.php
+++ wp-content/themes/international-prom/phpmodule_includes/EditProfileAccount/ModuleTextEditAccountForm.php
@@ -1,6 +1,8 @@
 <div class="section group">
-	<div class="col span_12_of_12">
+	<div class="col span_8_of_12">
 		<label for="store_name"><?php _e( 'Text Limit' ); ?> <span class="required">*</span></label>
 		<input type="text" name="text_limit" placeholder="" value="<?php echo get_user_meta( $user_ID, 'text_limit', true ); ?>" required="required"/>
 	</div>
+    <div class="col span_4_of_12">
+    </div>
 </div>
