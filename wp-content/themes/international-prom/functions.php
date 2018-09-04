811f3860ff77c6cfd7db12a3a1e0aa3424df6f4c
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-04 12:22:20 +0530

    Stripe Integration

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -833,7 +833,18 @@ add_action('wp_ajax_stripePayment', 'stripePayment');
 
 function stripePayment() {
     global $user_ID;
-    require_once(TEMPLATEPATH.'/stripe-php/config.php');
+	require_once 'vendor/autoload.php';
+	$stripe = array(
+		"secret_key"      => "sk_test_cmOSWkB6LfRQqP5wdO35ponC",
+		"publishable_key" => "pk_test_56lPSQy6Xz1qEz7z3OzxYzoJ"
+	);
+	/*$stripe = array(
+		"secret_key"      => "sk_live_8ML80gOt52EZFLFs5VVhmzHl",
+		"publishable_key" => "pk_live_WwyD4xmmjkMu62xLANjGkqRy"
+	);*/
+
+	\Stripe\Stripe::setApiKey($stripe['secret_key']);
+
     if ($_POST['stripeAmount']) {
         $amount   = $_POST['stripeAmount'] * 100;
         $token    = $_POST['stripeToken'];
