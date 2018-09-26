f91a987d0817ba0b0a637669dd6dc9454d46791a
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-26 13:19:28 +0530

    Stripe SDK integration

--- wp-content/themes/international-prom/functions.php
+++ wp-content/themes/international-prom/functions.php
@@ -833,15 +833,7 @@ add_action('wp_ajax_stripePayment', 'stripePayment');
 
 function stripePayment() {
     global $user_ID;
-	require_once 'vendor/autoload.php';
-	$stripe = array(
-		"secret_key"      => "sk_test_cmOSWkB6LfRQqP5wdO35ponC",
-		"publishable_key" => "pk_test_56lPSQy6Xz1qEz7z3OzxYzoJ"
-	);
-	/*$stripe = array(
-		"secret_key"      => "sk_live_8ML80gOt52EZFLFs5VVhmzHl",
-		"publishable_key" => "pk_live_WwyD4xmmjkMu62xLANjGkqRy"
-	);*/
+	require_once TEMPLATEPATH.'/stripe-php/config.php';
 
 	\Stripe\Stripe::setApiKey($stripe['secret_key']);
 
