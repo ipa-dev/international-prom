c6aed1ab71fd467785c2abb5d7fa0fbc439e9548
CoreTechSolution <CoreGenSaltlake1>
2018-12-28 19:28:14 +0530

    Integration of Style Number in CSV Import Export Plugin

--- wp-content/plugins/csv-import-export/includes/admin/views/esb-cie-import-export-page.php
+++ wp-content/plugins/csv-import-export/includes/admin/views/esb-cie-import-export-page.php
@@ -148,7 +148,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
                                             </tr>
 					    <tr class="row-additional-images">
 					    	<td><input type="checkbox" id="esb_cie_additional_images_gallery" name="esb_cie_column_name[]" value="additional-images" checked="checked" /></td>
-					    	<td class="row-title"><lable for="esb_cie_additional_images_gallery">Additional Images</label></td>
+					    	<td class="row-title"><label for="esb_cie_additional_images_gallery">Additional Images</label></td>
 					    	<td><code>additional-images</code></td>
 					    	<td><span class="description"></span</td>
 					    </tr>
